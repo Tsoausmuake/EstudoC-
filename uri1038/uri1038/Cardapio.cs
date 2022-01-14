b61ca26d1e2843b124f186ffb173ba0829dc4dce
Manoel Jorge <manoel.jmb1990@gmail.com>
2022-01-14 13:27:07 -0300

    cardápio salvo

---
+++ uri1038/uri1038/Cardapio.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace uri1038
+{
+    class Cardapio
+    {
+        string[] cardapio = new string[5] { "Cachorro Quente", "X-Salada", "X-Bacon", "Torrada Simples", "Refrigerante" };
+
+        string cachorroQuente = "Cachorro quente";
+
+        private String nome;
+        private int id;
+        private double valor;
+
+        public Cardapio() { }
+
+        public Cardapio(String nome, int id, double valor)
+        {
+            this.nome = nome;
+            this.id = id;
+            this.valor = valor;
+
+        }
+
+        public void setNome(String nome)
+        {
+            this.nome = nome;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+        public int getId()
+        {
+            return this.id;
+        }
+        public void setValor(double valor)
+        {
+            this.valor = valor;
+        }
+        public double getValor()
+        {
+            return this.valor;
+        }
+
+        public void testevet0()
+        {
+
+        }
+
+    }
+}
