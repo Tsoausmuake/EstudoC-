d11ff3117fd2800c5b59821b01d6a268bca478a6
Manoel Jorge <manoel.jmb1990@gmail.com>
2022-02-16 14:35:48 -0300

    16/02/2022

--- FuncoesString/FuncoesString/Program.cs
+++ FuncoesString/FuncoesString/Program.cs
@@ -6,7 +6,38 @@ namespace FuncoesString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string original = "abcde ABCDE ABC abc DEFG     ";
+
+            string s1 = original.ToUpper(); // converte tudo para maiusculo
+            string s2 = original.ToLower(); // converte tudo para minúsculo
+            string s3 = original.Trim(); // removo os espaços em branco, antes e depois da string;
+
+            int n1 = original.IndexOf("bc"); // mostra a primeira incidência do "bc" na string.
+            int n2 = original.LastIndexOf("bc"); // mostra a última incidência do bc na string 
+
+            string s4 = original.Substring(3); //corta da posição 3 em diante.
+            string s5 = original.Substring(3, 5); // corta da posição 3 em diante e apenas 5 caracteres.
+            string s6 = original.Replace('a', 'x'); // substituir todo caracter 'a' pelo caracter 'x'
+
+            bool b1 = string.IsNullOrEmpty(original); // Verificando se a variável original é nula ou vazia.
+            bool b2 = string.IsNullOrWhiteSpace(original); // vericando se a variável é null ou espaços em branco.
+
+            Console.WriteLine("Original: -" + original + "-");
+            Console.WriteLine("ToUpper: -" + s1 + "-"); //
+            Console.WriteLine("ToLower: -" + s2 + "-");
+            Console.WriteLine("Trim: -" + s3 + "-");
+            Console.WriteLine("IndexOf 'bc': " + n1 + "-");
+            Console.WriteLine("LastIndexOf 'bc': " + n2 + "-");
+            Console.WriteLine("SubsTring: " + s4 + "-");
+            Console.WriteLine("SubsTring: " + s5 + "-");
+            Console.WriteLine("Replace: " + s6 + "-");
+            Console.WriteLine("IsNullOrEmpty(original): " + b1);
+            Console.WriteLine("IsNullOrWhiteSpace(original): " + b2);
+
+
+
+
+
         }
     }
 }
---
+++ beecrowd1028/beecrowd1028/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace beecrowd1028
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i, m1, m2;
+
+            for (i = 0; i < n; i++)
+            {
+                string[] vet = Console.ReadLine().Split(' ');
+                int f1 = int.Parse(vet[0]);
+                int f2 = int.Parse(vet[1]);
+                m1 = f1;
+                m2 = f2;
+                bool test = true;
+                bool test2 = false;
+
+                while (test == true)
+                {
+                    if (test2 == true)
+                    { //fazendo um teste para dar entrada novamente nos valores, em caso de entrada de valores inválidos.
+
+                        vet = Console.ReadLine().Split(' ');
+                        f1 = int.Parse(vet[0]);
+                        f2 = int.Parse(vet[1]);
+                        Console.WriteLine("----------------------------");
+                    }
+                    //-----------------------------------------------------------------------------------------------------------------------------
+
+                    if (f1 % 2 == 0 && f2 % 2 == 0)
+                    {
+                        if (f1 > f2)
+                        {
+                            m2--;
+                            while (f2 % m2 != 0)
+                            {
+                                m2--;
+                                if (f2 % m2 == 0)
+                                {
+                                    Console.WriteLine(f2 / m2);
+
+                                }
+                                else
+                                {
+                                    m2--;
+                                }
+                            }
+                            test = false;
+                            test2 = false;
+                            Console.WriteLine("----------------------------");
+                        }
+                        else
+                        {
+
+                            m1--;
+
+                            while (f1 % m1 != 0)
+                            {
+                                m1--;
+                                if (f1 % m1 == 0)
+                                {
+                                    Console.WriteLine(f1 / m1);
+                                }
+
+
+                            }
+                            test = false;
+                            test2 = false;
+                            Console.WriteLine("----------------------------");
+                        }
+                    }
+                    else if (f1 % 2 != 0 && f2 % 2 != 0)
+                    {
+                        if (f1 > f2)
+                        {
+                            m2--;
+
+                            while (f2 % m2 != 0)
+                            {
+                                m2--;
+                                if (f2 % m2 == 0)
+                                {
+                                    Console.WriteLine(f2 / m2);
+                                }
+
+
+                            }
+                            test = false;
+                            test2 = false;
+                            Console.WriteLine("----------------------------");
+                        }
+                        else
+                        {
+
+                            m1--;
+
+                            while (f1 % m1 != 0)
+                            {
+                                m1--;
+                                if (f1 % m1 == 0)
+                                {
+                                    Console.WriteLine(f1 / m1);
+                                }
+
+
+                            }
+                            test = false;
+                            test2 = false;
+                            Console.WriteLine("----------------------------");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("----------------------------");
+                        Console.WriteLine("Dados Incompatíveis com o teste: ");
+                        Console.WriteLine($"Entre novamente com 2 valores inteiros: ");
+                        Console.WriteLine($"Ainda restam {n - i} entradas de dados: ");
+                        test = true;
+                        test2 = true;
+                        Console.WriteLine("----------------------------");
+                    }
+                }
+            }
+        }
+    }
+}
